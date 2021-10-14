8dbcd8e49f94393549866459b4c48e97a28b45ec
Ckala62rus <agr.akyla@mail.ru>
2021-10-14 22:01:35 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using MakeAuth.Data;
+using MakeAuth.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using (MyDbContext db = new MyDbContext())
+            {
+                var users = db.Users.ToList();
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/MyDbContext.cs
@@ -0,0 +1,43 @@
+﻿using MakeAuth.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeAuth.Data
+{
+    /// <summary>
+    /// 
+    /// Get-Migration - Список миграций
+    /// Update-Database Initial - откат миграции до конкретной миграции с именем Initial (Имя соответственно может быть любое)
+    /// Update-Database - применение миграций
+    /// Update-Database 0 - откат всех миграций.
+    /// Remove-Migration - удаляет файл последней миграции
+    /// Drop-Database - удаление БД
+    /// Add-Migration Test - создает новую миграцию с именем "Test"
+    /// 
+    /// </summary>
+    public class MyDbContext: DbContext
+    {
+        //public MyDbContext(DbContextOptions<MyDbContext> options): base(options)
+        //{
+        //    //
+        //}
+
+        public DbSet<User> Users { get; set; }
+
+        //public MyDbContext()
+        //{
+        //    Database.EnsureCreated();
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;user=root;password=root;database=sharp;",
+                new MySqlServerVersion(new Version(8, 0, 11))
+            );
+        }
+    }
+}
